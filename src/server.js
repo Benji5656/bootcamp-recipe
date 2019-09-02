6f0c8d31cfcbba686fe2e058bce5326a320096ef
[Benji] <benjamin_landis92@yahoo.com>
2019-09-02 10:39:51 -0500

    first

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const request = require('request')
+
+//serving files
+app.use(express.static(path.join(__dirname, '../dist')))
+app.use(express.static(path.join(__dirname, '../node_modules')))
+
+
+app.get('/', function(req, res) {
+    res.send("welcome welcome welcome")
+})
+
+
+const port = 3000
+app.listen(port, function () {
+    console.log(`Node server created at port ${port}`)
+})
